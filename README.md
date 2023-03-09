e5218c6bd3cfacf72ab8e2990b228df045d3a3f4
jonclawson <30538332+jonclawson@users.noreply.github.com>
2023-03-09 02:44:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-bmi-calculator
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-m1b1f8)
---
+++ index.js
@@ -0,0 +1,89 @@
+// Import stylesheets
+import './style.css';
+import "bootstrap/dist/css/bootstrap.css";
+import template from './template.html';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+appDiv.innerHTML = template;
+
+element('[name="metric"]').onclick = () => {
+ if (element('[name="metric"]').checked) {
+  element('#inches').classList.add('visually-hidden')
+  element('#height').textContent = "cm";
+  element('#mass').textContent = "kg"
+ }
+ else {
+  element('#inches').classList.remove('visually-hidden')
+  element('#height').textContent = "Feet";
+  element('#mass').textContent = "lbs"
+ }
+}
+
+element('[name="get"]').onclick = () => {
+  const metric = element('[name="metric"]').checked;
+  let height = +element('[name="height"]').value;
+  const inches = +element('[name="inches"]').value;
+  const weight = +element('[name="weight"]').value;
+  height = metric ? height / 100 : (height * 12) + inches
+
+  const bmi = getBmi(weight, height, metric);
+  const max = getBmiWeight(18.5, height, metric);
+  const min = getBmiWeight(25, height, metric);
+
+  element('#bmi').textContent = bmi.toLocaleString('en');
+  element('#target').textContent = `
+  ${Math.floor(max)} and ${Math.ceil(min)}
+  `;
+
+  element('#results').classList.remove('visually-hidden');
+
+  const spectrum = getSpectrum(height, metric);
+  const legend = [...document.querySelectorAll('#legend i')];
+  legend.forEach((li, i) => {
+    li.textContent = (`= (${spectrum[i].join(' - ')}) lbs`)
+  })
+}
+
+function getBmi (mass, height, metric) {
+  const bmi = mass / (height * height);
+  return metric ? bmi : 703 * bmi;
+}
+
+function getBmiWeight (bmi, height, metric) {
+  const weight = +bmi * (height * height);
+  return metric ? weight : (weight / 703);
+}
+
+function element (q) {
+  return document.querySelector(q)
+}
+
+function getSpectrum (height, metric) {
+  const h = +height;
+  const levels = getLevels();
+  const spectrum = [];
+  levels.forEach(l => {
+    spectrum.push(
+      [
+        Math.floor(getBmiWeight(l[0], h, metric)), 
+        Math.ceil(getBmiWeight(l[1], h, metric))
+      ]
+    )
+  })
+  return spectrum;
+}
+
+function getLevels () {
+  return [
+    [0, 15.8],
+    [16, 16.8],
+    [17, 18.3],
+    [18.5, 24.8],
+    [25, 29.8],
+    [30, 34.8],
+    [35, 39.8],
+    [40, 1000]
+  ]
+}
